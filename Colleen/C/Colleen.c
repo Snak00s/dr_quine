1614167b1e587da4e065d87a6b043038927d2635
Tom Pinton <tom.snak00s@gmail.com>
2026-04-30 16:51:27 +0200

    change comments and add makefile rm rules

--- Colleen/C/Colleen.c
+++ Colleen/C/Colleen.c
@@ -1,16 +1,19 @@
 #include<stdio.h>
 
-// yo
+/*
+	yo
+*/
 
 char *getReplica(void)
 {
-	return ("#include<stdio.h>%1$c%1$c// yo%1$c%1$cchar *getReplica(void)%1$c{%1$c%3$creturn (%2$c%4$s%2$c);%1$c}%1$c%1$cint main(void)%1$c{%1$c%1$c%3$c//issou%1$c%1$c%3$cchar *replica = getReplica();%1$c%3$cprintf(replica, 10, 34, 9, replica);%1$c%3$creturn (0);%1$c}");
+	return ("#include<stdio.h>%1$c%1$c/*%1$c%3$cyo%1$c*/%1$c%1$cchar *getReplica(void)%1$c{%1$c%3$creturn (%2$c%4$s%2$c);%1$c}%1$c%1$cint main(void)%1$c{%1$c%3$c/*%1$c%3$c%3$cissou%1$c%3$c*/%1$c%1$c%3$cchar *replica = getReplica();%1$c%3$cprintf(replica, 10, 34, 9, replica);%1$c%3$creturn (0);%1$c}");
 }
 
 int main(void)
 {
-
-	//issou
+	/*
+		issou
+	*/
 
 	char *replica = getReplica();
 	printf(replica, 10, 34, 9, replica);
--- Sully/C/Sully.c
+++ Sully/C/Sully.c
@@ -3,11 +3,11 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-char filename[500];
+char filename[100];
 
 char *getReplica(void)
 {
-	return("#include<fcntl.h>%1$c#include<unistd.h>%1$c#include<stdio.h>%1$c#include<stdlib.h>%1$c%1$cchar filename[500];%1$c%1$cchar *getReplica(void)%1$c{%1$c%2$creturn(%3$c%4$s%3$c);%1$c}%1$c%1$cint main(void)%1$c{%1$c%2$cint i = %5$d;%1$c%2$cif (i == 0)%1$c%2$c%2$creturn (0);%1$c%2$csprintf(filename, %3$cSully_%%d.c%3$c, --i);%1$c%2$cint fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 420);%1$c%2$cif (fd == -1)%1$c%2$c{%1$c%2$c%2$cprintf(%3$copen fail%3$c);%1$c%2$c%2$creturn (0);%1$c%2$c}%1$c%2$cchar *replica = getReplica();%1$c%2$cdprintf(fd, replica, 10, 9, 34, replica, i);%1$c%2$cclose(fd);%1$c%2$csprintf(filename, %3$ccc -Wall -Wextra -Werror Sully_%%1$d.c -o Sully_%%1$d%3$c, i);%1$c%2$csystem(filename);%1$c%2$csprintf(filename, %3$c./Sully_%%1$d%3$c, i);%1$c%2$csystem(filename);%1$c%2$creturn (0);%1$c}");
+	return("#include<fcntl.h>%1$c#include<unistd.h>%1$c#include<stdio.h>%1$c#include<stdlib.h>%1$c%1$cchar filename[100];%1$c%1$cchar *getReplica(void)%1$c{%1$c%2$creturn(%3$c%4$s%3$c);%1$c}%1$c%1$cint main(void)%1$c{%1$c%2$cint i = %5$d;%1$c%2$cif (i == 0)%1$c%2$c%2$creturn (0);%1$c%2$csprintf(filename, %3$cSully_%%d.c%3$c, --i);%1$c%2$cint fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 420);%1$c%2$cif (fd == -1)%1$c%2$c{%1$c%2$c%2$cprintf(%3$copen fail%3$c);%1$c%2$c%2$creturn (0);%1$c%2$c}%1$c%2$cchar *replica = getReplica();%1$c%2$cdprintf(fd, replica, 10, 9, 34, replica, i);%1$c%2$cclose(fd);%1$c%2$csprintf(filename, %3$ccc -Wall -Wextra -Werror Sully_%%1$d.c -o Sully_%%1$d%3$c, i);%1$c%2$csystem(filename);%1$c%2$csprintf(filename, %3$c./Sully_%%1$d%3$c, i);%1$c%2$csystem(filename);%1$c%2$creturn (0);%1$c}");
 }
 
 int main(void)
